276b70baae8578225dba2e862284082082a39db4
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-11 19:04:47 +0800

    Add files via upload

--- code/index.py
+++ code/index.py
@@ -13,6 +13,7 @@ db_name=os.environ['db_name']
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 #Connection
 try:
     connection = pymysql.connect(host=rds_endpoint, user=username,
@@ -24,24 +25,21 @@ except pymysql.MySQLError as e:
 logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def handler(event, context):
-    cur = connection.cursor()  
-## Retrieve Data    
-    query = "UPDATE Staff SET status='A' where id='{}'".format(event['id'])
-    cur.execute(query)
-    connection.commit()
-    print(cur.rowcount, "record(s) affected")
-## Construct body of the response object
-    transactionResponse = {}
-# Construct http response object
-    responseObject = {}
-    responseObject['statusCode'] = 200
-    responseObject['headers'] = {}
-    responseObject['headers']['Content-Type']='application/json'
-    responseObject['headers']['Access-Control-Allow-Origin']='*'
-    responseObject['body'] = json.dumps(transactionResponse, sort_keys=True,default=str)
-    
-    #k = json.loads(responseObject['body'])
-    #print(k['uin'])
-    #test
 
-    return responseObject
+## DDL database setup
+    cur = connection.cursor()
+    cur.execute("CREATE TABLE Customer (id int PRIMARY KEY AUTO_INCREMENT,email varchar(255) UNIQUE,password varchar(255),uin varchar(255) UNIQUE,name varchar(255),addr varchar(255), postal varchar(255), contactNo varchar(255))")
+    cur.execute("CREATE TABLE Clinic (id int PRIMARY KEY AUTO_INCREMENT,name varchar(255) UNIQUE)")
+    cur.execute("CREATE TABLE Branch (id int PRIMARY KEY AUTO_INCREMENT,name varchar(255) UNIQUE,district varchar(255),addr varchar(255),postal varchar(255),contactNo varchar(255),latt decimal(18,12),longt decimal(18,12),clinicId int,FOREIGN KEY(clinicId) REFERENCES Clinic(id))")
+    cur.execute("CREATE TABLE Staff (id int PRIMARY KEY AUTO_INCREMENT,email varchar(255) UNIQUE,password varchar(255),name varchar(255),addr varchar(255),postal varchar(255), contactNo varchar(255),job varchar(255),status varchar(1), isAdmin varchar(1), branchId int,FOREIGN KEY(branchId) REFERENCES Branch(id))")
+    cur.execute("CREATE TABLE OpeningHours (opens time,closes time,dayOfWeek int,branchId int,PRIMARY KEY(dayOfWeek, branchId),FOREIGN KEY(branchId) REFERENCES Branch(id))")
+    cur.execute("CREATE TABLE Queue (id int PRIMARY KEY AUTO_INCREMENT,status varchar(255),queueNumber int,createdDT date,customerId int,branchId int,FOREIGN KEY(customerId) REFERENCES Customer(id),FOREIGN KEY(branchId) REFERENCES Branch(id))")
+## Dummy data    
+    cur.execute("INSERT INTO Customer(email,password,uin,name,addr,contactNo) VALUES('customer1@hotmail.com','password','S1234567X','customer1','APPLE STREET','12345678')")
+    connection.commit()
+## Test dummy data retrieval    
+    cur.execute("SELECT * from Customer")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{0} {1} {2}".format(row[0],row[1],row[2]))
